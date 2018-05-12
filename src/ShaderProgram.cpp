1c342a28d19b3a55821b83da0108f320175adf95
Alexander Barber <alex@dangerzonegames.com>
2018-05-11 23:50:58 -0600

    Added documentation comments to ShaderProgram class.

--- src/ShaderProgram.cpp
+++ src/ShaderProgram.cpp
@@ -7,12 +7,27 @@
  * shader types to be executed by the renderer.
  */
 
+/**
+ * Default constructor. Allocates OpenGL shader program resource on GPU.
+ */
 tw::ShaderProgram::ShaderProgram () {
 
     m_id = glCreateProgram();
 
 }
 
+/**
+ * Constructor taking in optional std::shared_ptrs to one of each handle: vertex
+ * shader, fragment shader, geometry shader, tesselation evaluation shader,
+ * tesselation control shader, compute shader.
+ *
+ * @param vertexShader                vertex shader handle (optional) (pass nullptr to disable)
+ * @param fragmentShader              fragment shader handle (optional) (pass nullptr to disable)
+ * @param geometryShader              geometry shader handle (optional) (pass nullptr to disable)
+ * @param tesselationEvaluationShader tesselation evaluation shader handle (optional) (pass nullptr to disable)
+ * @param tesselationControlShader    tesselation control shader handle (optional) (pass nullptr to disable)
+ * @param computeShader               conpute shader handle (optional) (pass nullptr to disable)
+ */
 tw::ShaderProgram::ShaderProgram (
     const std::shared_ptr<VertexShader>& vertexShader,
     const std::shared_ptr<FragmentShader>& fragmentShader,
@@ -34,54 +49,108 @@ tw::ShaderProgram::ShaderProgram (
 
 }
 
+/**
+ * Get the ID of the shader program.
+ *
+ * @return shader program ID
+ */
 GLuint tw::ShaderProgram::getId () const {
 
     return m_id;
 
 }
 
+/**
+ * Check if the shader program has been linked.
+ *
+ * @return true if the shader program has been linked, false if not
+ */
 bool tw::ShaderProgram::isLinked () const {
 
     return m_linked;
 
 }
 
+/**
+ * Get a std::shared_ptr to the attached vertex shader. If no vertex shader is
+ * attached, returns nullptr.
+ *
+ * @return shared pointer to attached vertex shader, or nullptr if none
+ */
 std::shared_ptr<tw::VertexShader> tw::ShaderProgram::getVertexShader () const {
 
     return m_vertexShader;
 
 }
 
+/**
+ * Get a std::shared_ptr to the attached fragment shader. If no fragment shader
+ * is attached, returns nullptr.
+ *
+ * @return shared pointer to attached fragment shader, or nullptr if none
+ */
 std::shared_ptr<tw::FragmentShader> tw::ShaderProgram::getFragmentShader () const {
 
     return m_fragmentShader;
 
 }
 
+/**
+ * Get a std::shared_ptr to the attached geometry shader. If no geometry shader
+ * is attached, returns nullptr.
+ *
+ * @return shared pointer to attached geometry shader, or nullptr if none
+ */
 std::shared_ptr<tw::GeometryShader> tw::ShaderProgram::getGeometryShader () const {
 
     return m_geometryShader;
 
 }
 
+/**
+ * Get a std::shared_ptr to the attached tesselation evaluation shader. If no
+ * tesselation evaluation shader is attached, returns nullptr.
+ *
+ * @return shared pointer to attached tesselation evaluation shader, or nullptr if none
+ */
 std::shared_ptr<tw::TesselationEvaluationShader> tw::ShaderProgram::getTesselationEvaluationShader () const {
 
     return m_tesselationEvaluationShader;
 
 }
 
+/**
+ * Get a std::shared_ptr to the attached tesselation control shader. If no
+ * tesselation control shader is attached, returns nullptr.
+ *
+ * @return shared pointer to attached tesselation control shader, or nullptr if none
+ */
 std::shared_ptr<tw::TesselationControlShader> tw::ShaderProgram::getTesselationControlShader () const {
 
     return m_tesselationControlShader;
 
 }
 
+/**
+ * Get a std::shared_ptr to the attached compute shader. If no compute shader is
+ * attached, returns nullptr.
+ *
+ * @return shared pointer to attached compute shader, or nullptr if none
+ */
 std::shared_ptr<tw::ComputeShader> tw::ShaderProgram::getComputeShader () const {
 
     return m_computeShader;
 
 }
 
+/**
+ * Set the attached vertex shader to a given std::shared_ptr to a vertex shader
+ * handle. Only works if the program has not already been linked. You can pass
+ * nullptr to unset the vertex shader.
+ *
+ * @param vertexShader shared pointer to vertex shader handle
+ * @return true if vertex shader was successfully set, false if not
+ */
 bool tw::ShaderProgram::setVertexShader (const std::shared_ptr<VertexShader>& vertexShader) {
 
     if (isLinked()) {
@@ -94,6 +163,14 @@ bool tw::ShaderProgram::setVertexShader (const std::shared_ptr<VertexShader>& ve
 
 }
 
+/**
+ * Set the attached fragment shader to a given std::shared_ptr to a fragment
+ * shader handle. Only works if the program has not already been linked. You can
+ * pass nullptr to unset the fragment shader.
+ *
+ * @param fragmentShader shared pointer to fragment shader handle
+ * @return true if fragment shader was successfully set, false if not
+ */
 bool tw::ShaderProgram::setFragmentShader (const std::shared_ptr<FragmentShader>& fragmentShader) {
 
     if (isLinked()) {
@@ -106,6 +183,14 @@ bool tw::ShaderProgram::setFragmentShader (const std::shared_ptr<FragmentShader>
 
 }
 
+/**
+ * Set the attached geometry shader to a given std::shared_ptr to a geometry
+ * shader handle. Only works if the program has not already been linked. You can
+ * pass nullptr to unset the geometry shader.
+ *
+ * @param geometryShader shared pointer to geometry shader handle
+ * @return true if geometry shader was successfully set, false if not
+ */
 bool tw::ShaderProgram::setGeometryShader (const std::shared_ptr<GeometryShader>& geometryShader) {
 
     if (isLinked()) {
@@ -118,6 +203,15 @@ bool tw::ShaderProgram::setGeometryShader (const std::shared_ptr<GeometryShader>
 
 }
 
+/**
+ * Set the attached tesselation evaluation shader to a given std::shared_ptr to
+ * a tesselation evaluation shader handle. Only works if the program has not
+ * already been linked. You can pass nullptr to unset the tesselation
+ * evaluation shader.
+ *
+ * @param tesselationEvaluationShader shared pointer to tesselation evaluation shader handle
+ * @return true if tesselation evaluation shader was successfully set, false if not
+ */
 bool tw::ShaderProgram::setTesselationEvaluationShader (const std::shared_ptr<TesselationEvaluationShader>& tesselationEvaluationShader) {
 
     if (isLinked()) {
@@ -130,6 +224,14 @@ bool tw::ShaderProgram::setTesselationEvaluationShader (const std::shared_ptr<Te
 
 }
 
+/**
+ * Set the attached tesselation control shader to a given std::shared_ptr to a
+ * tesselation control shader handle. Only works if the program has not already
+ * been linked. You can pass nullptr to unset the tesselation control shader.
+ *
+ * @param tesselationControlShader shared pointer to tesselation control shader handle
+ * @return true if tesselation control shader was successfully set, false if not
+ */
 bool tw::ShaderProgram::setTesselationControlShader (const std::shared_ptr<TesselationControlShader>& tesselationControlShader) {
 
     if (isLinked()) {
@@ -142,6 +244,14 @@ bool tw::ShaderProgram::setTesselationControlShader (const std::shared_ptr<Tesse
 
 }
 
+/**
+ * Set the attached compute shader to a given std::shared_ptr to a compute
+ * shader handle. Only works if the program has not already been linked. You can
+ * pass nullptr to unset the compute shader.
+ *
+ * @param computeShader shared pointer to compute shader handle
+ * @return true if compute shader was successfully set, false if not
+ */
 bool tw::ShaderProgram::setComputeShader (const std::shared_ptr<ComputeShader>& computeShader) {
 
     if (isLinked()) {
@@ -154,6 +264,10 @@ bool tw::ShaderProgram::setComputeShader (const std::shared_ptr<ComputeShader>& 
 
 }
 
+/**
+ * Link all of the attached shaders in the shader program. Once this is done,
+ * the attached shaders cannot be modified.
+ */
 bool tw::ShaderProgram::link () {
 
     if (m_vertexShader != nullptr) {
@@ -205,6 +319,11 @@ bool tw::ShaderProgram::link () {
 
 }
 
+/**
+ * Set the shader program as the current program for use in rendering.
+ *
+ * @return true if shader program was successfully activated, false if not
+ */
 bool tw::ShaderProgram::use () {
 
     if (!isLinked()) {
@@ -219,6 +338,11 @@ bool tw::ShaderProgram::use () {
 
 }
 
+/**
+ * Disable the active shader program, equivalent to binding no shader.
+ *
+ * @return true if shader was successfully deactivated, false if not
+ */
 bool tw::ShaderProgram::unuse () {
 
     glUseProgram(0);
