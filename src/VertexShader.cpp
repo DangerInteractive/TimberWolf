3bda8593f7f099c9a4f056a952d68607b23fe355
Alexander Barber <alex@dangerzonegames.com>
2018-05-21 21:54:44 -0600

    Added documentation comments for VertexShader and Window classes, finishing the initial documentation effort!

--- src/VertexShader.cpp
+++ src/VertexShader.cpp
@@ -6,9 +6,17 @@
  * Handle for an OpenGL vertex shader (version 2.0+).
  */
 
+/**
+ * Default constructor.
+ */
 tw::VertexShader::VertexShader ()
 : Shader(GL_VERTEX_SHADER) {}
 
+/**
+ * Constructor taking in a std::string of vertex shader source code.
+ *
+ * @param source vertex shader source code
+ */
 tw::VertexShader::VertexShader (const std::string& source)
 : Shader(GL_VERTEX_SHADER, source) {
 
--- src/Window.cpp
+++ src/Window.cpp
@@ -7,6 +7,10 @@
  * manipulation to window properties.
  */
 
+/**
+ * Initialize the window. This must be done before opening the window. It
+ * binds error handlers and makes sure GLFW and OpenGL are ready and available.
+ */
 void tw::Window::init () {
 
     glfwSetErrorCallback([](int error, const char* description){
@@ -31,6 +35,11 @@ void tw::Window::init () {
 
 }
 
+/**
+ * Deinitialize the window. This must only be done after closing the window,
+ * and it cleans up GLFW and OpenGL resources. This is meant to be called only
+ * prior to terminating the program.
+ */
 void tw::Window::deinit () {
 
     glfwTerminate();
@@ -39,12 +48,21 @@ void tw::Window::deinit () {
 
 }
 
+/**
+ * Get a pointer to the GLFW window context, if available, or nullptr otherwise.
+ *
+ * @return pointer to the GLFW window context, or nullptr if not available
+ */
 GLFWwindow* tw::Window::getContext () {
 
     return m_context.get();
 
 }
 
+/**
+ * Make the window context current. This should be done prior to drawing to the
+ * window, as this informs OpenGL to draw to this window.
+ */
 void tw::Window::makeContextCurrent () {
 
     if (m_context != NULL) {
@@ -56,12 +74,20 @@ void tw::Window::makeContextCurrent () {
 
 }
 
+/**
+ * Check if the window is open.
+ *
+ * @return true if the window is open, false if not
+ */
 bool tw::Window::isOpen () {
 
     return m_context != nullptr;
 
 }
 
+/**
+ * Open the window. This should be done after calling tw::Window::init.
+ */
 void tw::Window::open () {
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, m_glVersionMajor);
@@ -101,6 +127,9 @@ void tw::Window::open () {
 
 }
 
+/**
+ * Close the window. This should be done before calling tw::Window::destroy.
+ */
 void tw::Window::close () {
 
     if (isOpen()) {
@@ -109,6 +138,10 @@ void tw::Window::close () {
 
 }
 
+/**
+ * Destroy the window context. This should be done after calling
+ * tw::Window::close and before calling tw::Window::deinit.
+ */
 void tw::Window::destroy () {
 
     if (isOpen()) {
@@ -117,6 +150,9 @@ void tw::Window::destroy () {
 
 }
 
+/**
+ * Clear the window between draws.
+ */
 void tw::Window::clear () {
 
     if (isOpen()) {
@@ -134,6 +170,11 @@ void tw::Window::clear () {
 
 }
 
+/**
+ * Clear the window between draws, given a clear color.
+ *
+ * @param color clear color
+ */
 void tw::Window::clear (const Color& color) {
 
     if (isOpen()) {
@@ -151,6 +192,15 @@ void tw::Window::clear (const Color& color) {
 
 }
 
+/**
+ * Clear the window between draws, given a clear color as red, green, blue, and
+ * alpha channels, respectively.
+ *
+ * @param r normalized value for red channel (0-1)
+ * @param g normalized value for green channel (0-1)
+ * @param b normalized value for blue channel (0-1)
+ * @param a normalized value for alpha channel (0-1)
+ */
 void tw::Window::clear (float r, float g, float b, float a) {
 
     if (isOpen()) {
@@ -172,18 +222,33 @@ void tw::Window::clear (float r, float g, float b, float a) {
 
 }
 
+/**
+ * Get the width of the window in pixels.
+ *
+ * @return width of window in pixels
+ */
 int tw::Window::getWidth () {
 
     return m_width;
 
 }
 
+/**
+ * Get the height of the window in pixels.
+ *
+ * @return height of window in pixels
+ */
 int tw::Window::getHeight () {
 
     return m_height;
 
 }
 
+/**
+ * Set the width of the window in pixels.
+ *
+ * @param new width of window in pixels
+ */
 void tw::Window::setWidth (const int width) {
 
     std::unique_lock<std::mutex> lock_size(mutex_size);
@@ -193,6 +258,11 @@ void tw::Window::setWidth (const int width) {
 
 }
 
+/**
+ * Set the height of the window in pixels.
+ *
+ * @param height new height of the window in pixels
+ */
 void tw::Window::setHeight (const int height) {
 
     std::unique_lock<std::mutex> lock_size(mutex_size);
@@ -202,6 +272,13 @@ void tw::Window::setHeight (const int height) {
 
 }
 
+/**
+ * Set the size of the window, given the width and height of the window in
+ * pixels, respectively.
+ *
+ * @param width  new width of the window
+ * @param height new height of the window
+ */
 void tw::Window::setSize (const int width, const int height) {
 
     std::unique_lock<std::mutex> lock_size(mutex_size);
@@ -212,12 +289,23 @@ void tw::Window::setSize (const int width, const int height) {
 
 }
 
+/**
+ * Get the title of the window.
+ *
+ * @return window title
+ */
 std::string tw::Window::getTitle () {
 
     return m_title;
 
 }
 
+/**
+ * Set the title of the window. This is what is shown in the OS window's title
+ * bar.
+ *
+ * @param title new window title
+ */
 void tw::Window::setTitle (const std::string& title) {
 
     std::unique_lock<std::mutex> lock_title(mutex_title);
@@ -227,18 +315,33 @@ void tw::Window::setTitle (const std::string& title) {
 
 }
 
+/**
+ * Get the major version of the OpenGL context for the window.
+ *
+ * @return OpenGL major version of context
+ */
 int tw::Window::getGlVersionMajor () {
 
     return m_glVersionMajor;
 
 }
 
+/**
+ * Get the minor version of the OpenGL context for the window.
+ *
+ * @return OpenGL minor version of context
+ */
 int tw::Window::getGlVersionMinor () {
 
     return m_glVersionMinor;
 
 }
 
+/**
+ * Get the version of OpenGL as a std::string.
+ *
+ * @return OpenGL version string of context
+ */
 std::string tw::Window::getGlVersion () {
 
     std::ostringstream oss;
@@ -247,6 +350,11 @@ std::string tw::Window::getGlVersion () {
 
 }
 
+/**
+ * Set the major version of the OpenGL context.
+ *
+ * @param majorVersion new OpenGL major version of the context
+ */
 void tw::Window::setGlVersionMajor (const int majorVersion) {
 
     std::unique_lock<std::mutex> lock_glVersion(mutex_glVersion);
@@ -259,6 +367,11 @@ void tw::Window::setGlVersionMajor (const int majorVersion) {
 
 }
 
+/**
+ * Set the minor version of the OpenGL context.
+ *
+ * @param minorVersion new OpenGL minor version of the context
+ */
 void tw::Window::setGlVersionMinor (const int minorVersion) {
 
     std::unique_lock<std::mutex> lock_glVersion(mutex_glVersion);
@@ -271,6 +384,13 @@ void tw::Window::setGlVersionMinor (const int minorVersion) {
 
 }
 
+/**
+ * Set the version of the OpenGL context given a major version and a minor
+ * version respectively.
+ *
+ * @param majorVersion new OpenGL major version of the context
+ * @param minorVersion new OpenGL minor version of the context
+ */
 void tw::Window::setGlVersion (const int majorVersion, const int minorVersion) {
 
     std::unique_lock<std::mutex> lock_glVersion(mutex_glVersion);
@@ -284,6 +404,9 @@ void tw::Window::setGlVersion (const int majorVersion, const int minorVersion) {
 
 }
 
+/**
+ * Inform GLFW of changes to the size of the window.
+ */
 void tw::Window::pushWindowSize () {
 
     if (m_context != NULL) {
@@ -292,6 +415,9 @@ void tw::Window::pushWindowSize () {
 
 }
 
+/**
+ * Inform GLFW of changes to the title of the window.
+ */
 void tw::Window::pushWindowTitle () {
 
     if (m_context != NULL) {
@@ -300,6 +426,19 @@ void tw::Window::pushWindowTitle () {
 
 }
 
+/**
+ * Callback for OpenGL errors, which functions only for OpenGL 4.3+. This is a
+ * major improvement in OpenGL error logging, as it does not require a costly
+ * request for logged error messages, and messages are logged instantly.
+ *
+ * @param source    source of the OpenGL error
+ * @param type      type of the OpenGL error
+ * @param id        ID of the OpenGL error
+ * @param severity  severity of the OpenGL error
+ * @param length    length of the message
+ * @param message   message text
+ * @param userParam user parameter passed during binding (should be nullptr)
+ */
 void tw::Window::glLiveDebug (
     GLenum source,
     GLenum type,
@@ -327,18 +466,18 @@ void tw::Window::glLiveDebug (
 
 }
 
-std::unique_ptr<GLFWwindow, tw::Window::GLFWwindowDeleter> tw::Window::m_context;
+std::unique_ptr<GLFWwindow, tw::Window::GLFWwindowDeleter> tw::Window::m_context; ///< GLFW window context
 
-int tw::Window::m_width = 800;
-int tw::Window::m_height = 600;
-std::mutex tw::Window::mutex_size;
+int tw::Window::m_width = 800; ///< width of the window
+int tw::Window::m_height = 600; ///< height of the window
+std::mutex tw::Window::mutex_size; ///< mutex for window height and width values
 
-std::string tw::Window::m_title = "";
-std::mutex tw::Window::mutex_title;
+std::string tw::Window::m_title = ""; ///< window title
+std::mutex tw::Window::mutex_title; ///< mutex for the window title
 
-int tw::Window::m_glVersionMajor = 2;
-int tw::Window::m_glVersionMinor = 1;
-std::mutex tw::Window::mutex_glVersion;
+int tw::Window::m_glVersionMajor = 2; ///< major version of OpenGL context
+int tw::Window::m_glVersionMinor = 1; ///< minor version of OpenGL context
+std::mutex tw::Window::mutex_glVersion; ///< mutex for the OpenGL version
 
-tw::Color tw::Window::m_lastClearColor {0.f, 0.f, 0.f, 1.f};
-const tw::Color tw::Window::m_defaultClearColor {0.f, 0.f, 0.f, 1.f};
+tw::Color tw::Window::m_lastClearColor {0.f, 0.f, 0.f, 1.f}; ///< last color used to clear the window
+const tw::Color tw::Window::m_defaultClearColor {0.f, 0.f, 0.f, 1.f}; ///< default color for clearing the window
