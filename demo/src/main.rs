d40fdcbcca6ef72764064234eafd5c8cddf130e9
Alexander Barber <alex@dangerzonegames.com>
2019-06-15 01:14:01 -0600

    :alembic: working on the demo game

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,10 +1,39 @@
 extern crate timberwolf;
 
-use timberwolf::{Game};
+use timberwolf::{Game, context::{Context, Request}, log::{Log}};
 
 fn main () {
 
-    let game = Game::new();
+    let mut game = Game::new();
+    game.push_story_context(Box::new(LoadingContext::new()));
     game.run(60, 20);
+
+}
+
+struct LoadingContext;
+impl LoadingContext {
+
+    fn new () -> Self {
+        return Self;
+    }
+
+}
+impl Context for LoadingContext {
+
+    fn is_render_transparent (&self) -> bool { false }
+    fn is_update_transparent (&self) -> bool { false }
+    fn is_input_transparent  (&self) -> bool { false }
+
+    fn render (&self, delta: f64, log: &Log) -> Request {
+        let message = format!("render delta: {}", delta);
+        log.verbose("demo", &message);
+        Request::Continue
+    }
+
+    fn update (&mut self, delta: f64, log: &Log) -> Request {
+        let message = format!("update delta: {}", delta);
+        log.verbose("demo", &message);
+        Request::Continue
+    }
 
 }
