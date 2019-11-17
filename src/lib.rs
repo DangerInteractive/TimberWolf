1bba46f22467fa6baced3247283c813b0f68bbbd
Alexander Barber <alex@dangerzonegames.com>
2019-11-16 19:24:01 -0700

    :rotating_light: make the linter more strict

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! TimberWolf -  A high-performance 3D focused cross-platform game engine based on Rust
 
-#![warn(dead_code)]
-#![warn(missing_docs)]
+#![deny(dead_code)]
+#![deny(missing_docs)]
 
 pub mod color;
 pub mod input;
